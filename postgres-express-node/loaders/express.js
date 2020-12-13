b5767f75ffe19b3b3fe7d4c153446b74738eb1b9
MislavKovac <mkovac07@fesb.hr>
2020-12-13 17:37:46 +0100

    Invalid token message implemented

--- postgres-express-node/loaders/express.js
+++ postgres-express-node/loaders/express.js
@@ -38,7 +38,7 @@ module.exports = ({ app, HttpLogger: logger }) => {
 
   // ultimate error handler
   app.use((err, req, res, next) => {
-    if(err.name === "Unathorized Error"){
+    if(err.name === "UnauthorizedError"){
       err.status = 401;
       err.message = "Not authorized (invalid token)";
     }
